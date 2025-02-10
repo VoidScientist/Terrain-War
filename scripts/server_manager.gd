174efb05a0ac6bd83bd1861c7ba432c2123aaddd
VoidScientist <louthosi59@gmail.com>
2025-02-10 17:33:57 +0100

    handled server disconnection case

--- scripts/server_manager.gd
+++ scripts/server_manager.gd
@@ -14,14 +14,20 @@ func create_client():
 		
 	get_tree().network_peer = peer
 	
-	peer.connect("connection_succeeded", self, "on_connection_succeeded")
+	peer.connect("connection_succeeded", self, "_on_connection_succeeded")
 	peer.connect("connection_failed", self, "_on_connection_failed")
+	peer.connect("server_disconnected", self, "_on_server_disconnected")
 	
 	return true
 	
 func _on_connection_failed():
+	connected = false
 	print("failed to connect to server")
 	
 func _on_connection_succeeded():
 	connected = true
 	print("connected to server")
+	
+func _on_server_disconnected():
+	connected = false
+	print("lost connection with server")
